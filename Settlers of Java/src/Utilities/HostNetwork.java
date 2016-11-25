4e4c892e2490be8b13dc73231234748469786912
Adam <adam.van.hal@ku.edu>
2016-11-25 14:33:32 -0600

    Adding network functions

--- Settlers of Java/src/Utilities/HostNetwork.java
+++ Settlers of Java/src/Utilities/HostNetwork.java
@@ -1,7 +1,18 @@
 package Utilities;
 
-public class HostNetwork {
+import java.net.*;
 
+public class HostNetwork {
+	private int port;
+	private String hostName;
+	private String gameName;
+	
+	public HostNetwork(int portNumber, String hostName, String gameName){
+		this.port=portNumber;
+		this.hostName = hostName;
+		this.gameName = gameName;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
