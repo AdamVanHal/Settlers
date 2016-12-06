aa52bafc270432ae3fce2379ca08a5e131ae3af9
Adam <adam.van.hal@ku.edu>
2016-12-06 14:37:02 -0600

    Added networking to the Host GUI

--- Settlers of Java/src/gui/HostWindow.java
+++ Settlers of Java/src/gui/HostWindow.java
@@ -2,8 +2,11 @@ package gui;
 
 import java.awt.EventQueue;
 import java.awt.event.*;
+import java.io.IOException;
 
 import javax.swing.*;
+
+import Utilities.HostNetwork;
 
 /**
  * @file LaunchWindow.java
@@ -57,6 +60,8 @@ public class HostWindow {
 	 * @return None
 	 */
 	private void initialize() {
+		HostNetwork Host;
+		Host = new HostNetwork(5000, LaunchWindow.username, "New Game");
 		frame = new JFrame("Create and Host a Game");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -83,6 +88,43 @@ public class HostWindow {
 				});
 			}
 		});
+		
+		JLabel lblPlayerCount = new JLabel("# Players");
+		JTextField txtPlayerCount = new JTextField("3");
+		JLabel lblPort = new JLabel("Port #");
+		JTextField txtPort = new JTextField("5000");
+		JButton btnOpen = new JButton("Open Server");
+		btnOpen.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				int PortNumber = 5000;
+				try {
+					PortNumber = Integer.parseInt(txtPort.getText());
+				} catch (NumberFormatException e1) {
+					e1.printStackTrace();
+				}
+				Host.Port(PortNumber);
+				int PlayerNum = 3;
+				try {
+					PlayerNum = Integer.parseInt(txtPlayerCount.getText());
+				} catch (NumberFormatException e1) {
+					e1.printStackTrace();
+				}
+				try {
+					Host.StartHost();
+				} catch (IOException e) {
+					//we failed to start
+					e.printStackTrace();
+				}
+				try {
+					Host.addClients(PlayerNum-1);
+				} catch (IOException e) {
+					//Connection Failed
+					e.printStackTrace();
+				}
+			}
+		});
+		
 		//btnKick1 wouldnt be used, temporarily commented
 		/*
 		JButton btnKick1 = new JButton("Kick");
@@ -143,17 +185,24 @@ public class HostWindow {
 		
 		Title.setBounds(125,0,200,25);
 		btnStart.setBounds(175,200,100,25);
-		//btnKick1.setBounds(315,50,60,25);
-		btnKick2.setBounds(315,75,60,25);
-		btnKick3.setBounds(315,100,60,25);
-		btnKick4.setBounds(315,125,60,25);
-		Player1.setBounds(75,50,240,25);
+		
+		lblPlayerCount.setBounds(75, 50, 60, 25);
+		txtPlayerCount.setBounds(135, 50, 35, 25);
+		lblPort.setBounds(170, 50, 50, 25);
+		txtPort.setBounds(220, 50, 50, 25);
+		btnOpen.setBounds(315,50,110,25);
+		
+		//btnKick1.setBounds(315,75,60,25);
+		btnKick2.setBounds(315,100,60,25);
+		btnKick3.setBounds(315,125,60,25);
+		btnKick4.setBounds(315,150,60,25);
+		Player1.setBounds(75,75,240,25);
 		Player1.setEditable(false);
-		Player2.setBounds(75,75,240,25);
+		Player2.setBounds(75,100,240,25);
 		Player2.setEditable(false);
-		Player3.setBounds(75,100,240,25);
+		Player3.setBounds(75,125,240,25);
 		Player3.setEditable(false);
-		Player4.setBounds(75,125,240,25);
+		Player4.setBounds(75,150,240,25);
 		Player4.setEditable(false);
 		
 		lblName.setBounds(0,20,450,25);
@@ -169,6 +218,13 @@ public class HostWindow {
 		Status.add(btnStart);
 		Status.add(Title);
 		Status.add(lblName);
+		
+		Status.add(lblPlayerCount);
+		Status.add(txtPlayerCount);
+		Status.add(lblPort);
+		Status.add(txtPort);
+		Status.add(btnOpen);
+		
 		//Status.add(btnKick1);
 		Status.add(btnKick2);
 		Status.add(btnKick3);
